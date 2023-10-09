7a04a0cb01b38c5141f1880b646cbde18b24b941
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-10-08 22:16:39 -0300

    feat: testes de integração e unitarios -> post /booking

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -3,10 +3,11 @@ import { cleanDb, generateValidToken } from "../helpers";
 import supertest from "supertest";
 import httpStatus from "http-status";
 import faker from "@faker-js/faker";
-import { createUser } from "../factories";
+import { createEnrollmentWithAddress, createTicket, createTicketType, createUser } from "../factories";
 import * as jwt from 'jsonwebtoken';
 import { createBooking } from "../factories/booking-factory";
 import { createHotel, createRoomWithHotelId } from "../factories/hotels-factory";
+import { TicketStatus } from "@prisma/client";
 
 beforeAll(async () => {
     await init();
@@ -14,6 +15,10 @@ beforeAll(async () => {
   
 beforeEach(async () => {
     await cleanDb();
+});
+
+beforeEach(() => {
+    jest.clearAllMocks();
 });
 
 const server = supertest(app)
@@ -70,4 +75,49 @@ describe("GET /booking", ()=>{
 
 })
 
+describe("POST /booking", ()=> {
+
+    it('Should respond with status 401 if no token is given', async ()=> {
+        const result = await server.post("/booking")
+
+        expect(result.statusCode).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    it('Should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word()
+
+        const result = await server.post("/booking").set('Authorization', `Bearer ${token}`);
+
+        expect(result.statusCode).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    it('Should respond with status 401 if given token doesnt match any session', async ()=>{
+        const user = await createUser()
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+        const result = await server.post("/booking").set('Authorization', `Bearer ${token}`);
+        expect(result.statusCode).toBe(httpStatus.UNAUTHORIZED)
+    })
+
+    describe("When token is valid", () => {
+        it("Should respond with status 200 and a valid body", async ()=>{
+            const user = await createUser()
+            const token = await generateValidToken(user)
+            const hotel = await createHotel()
+            const room = await createRoomWithHotelId(hotel.id)
+            const enrollment = await createEnrollmentWithAddress(user);
+            const ticketType = await createTicketType(false, true);
+            const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+
+            const result = await server.post("/booking").set('Authorization', `Bearer ${token}`).send({roomId: room.id});
+
+            expect(result.statusCode).toBe(httpStatus.OK)
+            expect(result.body).toEqual({
+                bookingId: expect.any(Number)
+            })
+        })
+    })
+
+})
 
--- tests/unit/booking-unit.test.ts
+++ tests/unit/booking-unit.test.ts
@@ -1,9 +1,12 @@
 import app, { init } from "@/app";
-import { cleanDb } from "../helpers";
+import { cleanDb, generateValidToken } from "../helpers";
 import supertest from "supertest";
-import { bookingRepository } from "@/repositories";
+import { bookingRepository, enrollmentRepository, ticketsRepository } from "@/repositories";
 import { bookingService } from "@/services";
 import httpStatus from "http-status";
+import { createHotel, createRoomWithHotelId } from "../factories/hotels-factory";
+import { createEnrollmentWithAddress, createTicket, createTicketType, createUser } from "../factories";
+import { TicketStatus } from "@prisma/client";
 
 beforeAll(async () => {
     await init();
@@ -13,11 +16,15 @@ beforeEach(async () => {
     await cleanDb();
 });
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 const server = supertest(app)
 
 describe("GET /booking", ()=> {
 
-    it("Should respond with status 400 if user doesnt have a booking", async ()=>{
+    it("Should respond with status 404 if user doesnt have a booking", async ()=>{
 
         jest.spyOn(bookingRepository, "getBookingByUserId").mockImplementationOnce((): any => {
             return undefined;
@@ -32,4 +39,175 @@ describe("GET /booking", ()=> {
 
     })
 
+})
+
+describe("POST /booking", ()=> {
+
+    it("Should respond with status 404 room doenst exist", async ()=>{
+
+        jest.spyOn(bookingRepository, "getRoomById").mockImplementationOnce((): any => {
+            return undefined
+        })
+        jest.spyOn(bookingRepository, "countBookingsByRoom").mockImplementationOnce((): any => {
+            return 2;
+        })
+        jest.spyOn(enrollmentRepository, "findWithAddressByUserId").mockImplementationOnce((): any => {
+            return {
+                id: 1
+            }
+        })
+        jest.spyOn(ticketsRepository, "findTicketByEnrollmentId").mockImplementationOnce((): any => {
+            return {
+                status: "PAID",
+                TicketType: {
+                    isRemote: false,
+                    includesHotel: true,
+                }
+            };
+        })
+
+        const result = bookingService.postBooking(10, 10)
+
+        expect(result).rejects.toEqual({
+            name: 'NotFoundError',
+            message: 'No result for this search!',
+        })
+
+    })
+
+    it("It should respond with status 403 if the room is full", async ()=>{
+
+        jest.spyOn(bookingRepository, "getRoomById").mockImplementationOnce((): any => {
+            return {
+                capacity: 0
+            }
+        })
+        jest.spyOn(bookingRepository, "countBookingsByRoom").mockImplementationOnce((): any => {
+            return 2;
+        })
+        jest.spyOn(enrollmentRepository, "findWithAddressByUserId").mockImplementationOnce((): any => {
+            return {
+                id: 1
+            }
+        })
+        jest.spyOn(ticketsRepository, "findTicketByEnrollmentId").mockImplementationOnce((): any => {
+            return {
+                status: "PAID",
+                TicketType: {
+                    isRemote: false,
+                    includesHotel: true,
+                }
+            };
+        })
+
+        const result = bookingService.postBooking(10, 10)
+
+        expect(result).rejects.toEqual({
+            name: 'Forbidden',
+            message: 'This action violates a business rule',
+        })
+
+    })
+
+    it("It should respond with status 403 if ticket is remote", async ()=>{
+
+        jest.spyOn(bookingRepository, "getRoomById").mockImplementationOnce((): any => {
+            return {
+                capacity: 4
+            }
+        })
+        jest.spyOn(bookingRepository, "countBookingsByRoom").mockImplementationOnce((): any => {
+            return 2;
+        })
+        jest.spyOn(enrollmentRepository, "findWithAddressByUserId").mockImplementationOnce((): any => {
+            return {
+                id: 1
+            }
+        })
+        jest.spyOn(ticketsRepository, "findTicketByEnrollmentId").mockImplementationOnce((): any => {
+            return {
+                status: "PAID",
+                TicketType: {
+                    isRemote: true,
+                    includesHotel: true,
+                }
+            };
+        })
+
+        const result = bookingService.postBooking(10, 10)
+
+        expect(result).rejects.toEqual({
+            name: 'Forbidden',
+            message: 'This action violates a business rule',
+        })
+
+    })
+
+    it("It should respond with status 403 if ticket doesnt include hotel", async ()=>{
+
+        jest.spyOn(bookingRepository, "getRoomById").mockImplementationOnce((): any => {
+            return {
+                capacity: 4
+            }
+        })
+        jest.spyOn(bookingRepository, "countBookingsByRoom").mockImplementationOnce((): any => {
+            return 2;
+        })
+        jest.spyOn(enrollmentRepository, "findWithAddressByUserId").mockImplementationOnce((): any => {
+            return {
+                id: 1
+            }
+        })
+        jest.spyOn(ticketsRepository, "findTicketByEnrollmentId").mockImplementationOnce((): any => {
+            return {
+                status: "PAID",
+                TicketType: {
+                    isRemote: false,
+                    includesHotel: false,
+                }
+            };
+        })
+
+        const result = bookingService.postBooking(10, 10)
+
+        expect(result).rejects.toEqual({
+            name: 'Forbidden',
+            message: 'This action violates a business rule',
+        })
+
+    })
+
+    it("It should respond with status 403 if ticket isnt paid", async ()=>{
+
+        jest.spyOn(bookingRepository, "getRoomById").mockImplementationOnce((): any => {
+            return {
+                capacity: 4
+            }
+        })
+        jest.spyOn(bookingRepository, "countBookingsByRoom").mockImplementationOnce((): any => {
+            return 2;
+        })
+        jest.spyOn(enrollmentRepository, "findWithAddressByUserId").mockImplementationOnce((): any => {
+            return {
+                id: 1
+            }
+        })
+        jest.spyOn(ticketsRepository, "findTicketByEnrollmentId").mockImplementationOnce((): any => {
+            return {
+                status: "RESERVED",
+                TicketType: {
+                    isRemote: false,
+                    includesHotel: true,
+                }
+            };
+        })
+
+        const result = bookingService.postBooking(10, 10)
+
+        expect(result).rejects.toEqual({
+            name: 'Forbidden',
+            message: 'This action violates a business rule',
+        })
+
+    })
 })
